fdee96d79388d8cb3fe798c812898ae00159a7b2
CrisH <cristopher.26hp@gmail.com>
2024-11-06 09:59:32 -0600

    first commit

---
+++ src/products/products.service.ts
@@ -0,0 +1,42 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class ProductsService {
+
+  constructor(private prismaService: PrismaService) {}
+
+  create(createProductDto: CreateProductDto) {
+    return this.prismaService.product.create({
+      data: createProductDto,
+    })
+  }
+
+  findAll() {
+    return this.prismaService.product.findMany();
+  }
+
+  async findOne(id: number) {
+    const productFound = await this.prismaService.product.findUnique({
+      where: {
+        id: id,
+      }
+    })
+
+    if(!productFound) {
+      throw new NotFoundException(`Product with id ${id} not found`);
+    }
+
+    return productFound;
+  }
+
+  update(id: number, updateProductDto: UpdateProductDto) {
+    return `This action updates a #${id} product`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} product`;
+  }
+}
